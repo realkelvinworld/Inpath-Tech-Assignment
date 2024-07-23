73aaee7c49ee0676c5e2d66d5aea45abaebe5c37
realkelvinworld <4231210031@live.gctu.edu.gh>
2024-07-23 17:46:35 +0000

    kk

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Search } from "lucide-react";
+
+function SearchBar() {
+  return (
+    <main className="mt-5">
+      <section className="flex items-center justify-between px-3 py-2 lg:w-[600px] w-[380px] border rounded-full bg-white">
+        <div className="flex items-center">
+          <span>
+            <Search className="h-6 w-6 text-slate-200" />
+          </span>
+          <Input
+            type="text"
+            placeholder={`Try "Civil Engineer"`}
+            className="dark:bg- bg-white text-black border-none outline-none focus:outline-none lg:w-[330px] w-[200px]"
+          />
+        </div>
+        <div className="border-l flex item ">
+          <Select>
+            <SelectTrigger className="lg:w-[180px] w-[100px] bg-transparent border-none text-slate-400 dark:bg- bg-white">
+              <SelectValue placeholder="Jobs" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Jobs</SelectLabel>
+                <SelectItem value="apple">Civil Engineering</SelectItem>
+                <SelectItem value="banana">Doctor</SelectItem>
+                <SelectItem value="blueberry">Electrician</SelectItem>
+                <SelectItem value="grapes">Clerk</SelectItem>
+                <SelectItem value="pineapple">Teacher</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <div>
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full bg-green-700 hover:bg-green-500"
+            >
+              <ArrowRight className="h-4 w-4 text-white" />
+            </Button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default SearchBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import SearchBar from "../app/components/SearchBar"; // Corrected spelling
 
 export default function Home() {
   return (
-    <main className=" herobg1 h-[100vh]">
-      <h1>Hello</h1>
+    <main className="herobg1 h-[100vh] flex  justify-center px-6">
+      <section className=" flex flex-col items-start mt-56">
+        <div className="flex flex-col items-start">
+          <h1 className="font-black text-6xl">Discover a greater you.</h1>
+          <p className="font-bold text-4xl mt-4">
+            We help you{" "}
+            <span className="text-green-700 dark:text-green-500">
+              find the perfect job
+            </span>{" "}
+          </p>
+        </div>
+        <SearchBar />
+      </section>
     </main>
   );
 }
